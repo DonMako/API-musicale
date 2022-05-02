2a29938a568a02816f73c4b5cb8bac4006bc9125
Lucas Macaux <lucas0203@orange.fr>
2022-05-02 22:51:53 +0200

    Mise à jour de la création de playlist

--- frontend/getPlaylist.py
+++ frontend/getPlaylist.py
@@ -2,5 +2,8 @@ import json
 from backend.createPlaylist import createPlaylist
 
 
-def getPlaylist(file: json, len_playlist: int) -> json:
-    return createPlaylist(file, len_playlist)
+def getPlaylist(file: json, len_playlist: int) -> None:
+    data_file = open("playlist.json", "w")
+    playlist = createPlaylist(file)
+    json.dump(playlist, data_file, indent="")
+    data_file.close()
