ad6f2e0d229d796e20177d550b153fac78323099
Lucas Macaux <lucas0203@orange.fr>
2022-05-02 17:43:13 +0200

    Correction d'une erreur mineure

--- backend/selectSongsFromFile.py
+++ backend/selectSongsFromFile.py
@@ -7,8 +7,8 @@ from backend.getRandomTrackArtist import getRandomTrackArtist
 def selectSongsFromFile(file: json) -> List[dict]:
     list_songs = []
     for artist in file:
-		song = getRandomTrackArtist(artist["artiste"])
-		song["name_artist"] = artist["artiste"], 
-		song["id_artist"] = getIdArtist(artist["artiste"])
-		list_songs.append(song)
-	return list_songs
+        song = getRandomTrackArtist(artist["artiste"])
+        song["name_artist"] = artist["artiste"]
+        song["id_artist"] = getIdArtist(artist["artiste"])
+        list_songs.append(song)
+    return list_songs
--- main.py
+++ main.py
@@ -6,10 +6,10 @@ app = FastAPI()
 
 
 @app.get("/")
-def read_root():
+def read_root() -> dict:
     return {"message": "Salut, monde !"}
 
 
 @app.get("/random/{artist_name}")
-def get_all_words(artist_name: Optional[str]):
+def get_all_words(artist_name: Optional[str]) -> dict:
     return getRandomTrackArtist(artist_name)
