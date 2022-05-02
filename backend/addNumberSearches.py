1b9f369e1abccd05d6be9441d76f7c3e2aebb61e
Lucas Macaux <lucas0203@orange.fr>
2022-05-02 18:19:33 +0200

    Correction d'une erreur d'encodage

--- backend/addNumberSearches.py
+++ backend/addNumberSearches.py
@@ -4,6 +4,6 @@ from backend.totalScore import totalScore
 
 def addNumberSearches(file: json, len_playlist: int) -> None:
     total_score = totalScore(file)
-    for i in len(file):
+    for i in range(len(file)):
         file[i]["numberSearches"] = round(
             file[i]["note"]/total_score*len_playlist)
