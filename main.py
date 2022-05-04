5045a248c3742e540b5f82521048f1e83de561ba
Lucas Macaux <lucas0203@orange.fr>
2022-05-04 15:11:01 +0200

    Màj du README.md

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 from client.createPlaylist.getPlaylist import getPlaylist
 from client.getTrack import getTrack
 
-print('What do you want ?\n1) Get a playlist\n2) Get a song from an artist')
+print("What do you want ?\n1) Get a playlist\n2) Get a song from an artist\n3) Lauch the 'Rudy' scenario")
 x = input()
-if x == "1":
+if "1" in x:
     print('Name of the file used:')
     name = input()
     print('Number of songs desired:')
     number = input()
     print(getPlaylist(name, number))
-else:
+elif "2" in x:
     print("Artist's name:")
     name = input()
     print(getTrack(name))
+else:
+    print(getPlaylist(rudy.json, 20))
