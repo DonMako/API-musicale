55a432b465dba4706c569489b95073e4b20d177a
DonMako <80451849+DonMako@users.noreply.github.com>
2024-12-21 12:34:28 +0100

    Améliorer les test d'input de l'utilisateur
    Correction d'une erreur d'implémentation : si l'utilisateur rentre "13", le premier test était vrai, alors que l'utilisateur n'a pas entré la bonne séquence

--- main.py
+++ main.py
@@ -1,20 +1,27 @@
 from client.createPlaylist.getPlaylist import getPlaylist
 from client.getTrack import getTrack
 
-print("What do you want ?")
-print("1) Get a playlist")
-print("2) Get a song from an artist")
-print("3) Lauch the 'Rudy' scenario")
+def printOptions():
+    print("What do you want ?")
+    print("1) Get a playlist")
+    print("2) Get a song from an artist")
+    print("3) Lauch the 'Rudy' scenario")
+
+printOptions()
 x = input()
-if "1" in x:
+if "1" == x:
     print('Name of the file used:')
     name = input()
     print('Number of songs desired:')
     number = input()
     print(getPlaylist(name, number))
-elif "2" in x:
+elif "2" == x:
     print("Artist's name:")
     name = input()
     print(getTrack(name))
-else:
+elif "3" == x:
+    print("'Rudy' scenario launched")
     print(getPlaylist("rudy.json", 20))
+else:
+    print("This option isn't available yet")
+    printOptions()
