6434d47b070c6f9fbef79779808f02d696658696
DonMako <80451849+DonMako@users.noreply.github.com>
2025-11-09 22:12:24 +0100

    Empty getIdArtist.py

--- APIMusicale/AudioDB/getIdArtist.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-
-def getIdArtist(name_artist: str) -> int:
-    requete = requests.get(
-        "https://theaudiodb.com/api/v1/json/2/search.php?s=" + name_artist
-    ).json()
-    return requete["artists"][0]["idArtist"]
