060e9958f640f30e8eebd6404455d035a9412735
DonMako <80451849+DonMako@users.noreply.github.com>
2025-11-09 22:12:59 +0100

    Put getIdDiscography into AudioDB.py

--- APIMusicale/AudioDB.py
+++ APIMusicale/AudioDB.py
@@ -1,4 +1,17 @@
 import requests
+from typing import List
+
+
+def getIdDiscography(id_artist: int) -> List[int]:
+    requete = requests.get(
+        "https://theaudiodb.com/api/v1/json/{APIKEY}/album.php?i=" + str(
+            id_artist)
+    ).json
+    list_albums = requete["album"]
+    list_id_albums = []
+    for album in list_albums:
+        list_id_albums.append(album["idAlbum"])
+    return list_id_albums
 
 def getIdArtist(name_artist: str) -> int:
     requete = requests.get(
