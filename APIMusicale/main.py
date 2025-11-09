1af400b237c09b6e9a05666d9f369864487aaab2
DonMako <80451849+DonMako@users.noreply.github.com>
2025-11-09 22:19:49 +0100

    Change due to creation of AudioDB.py

--- APIMusicale/main.py
+++ APIMusicale/main.py
@@ -2,11 +2,7 @@ import random
 from typing import List
 from fastapi import FastAPI
 import uvicorn
-from AudioDB.getIdArtist import getIdArtist
-from AudioDB.getIdDiscography import getIdDiscography
-from AudioDB.getTracksAlbum import getTracksAlbum
-from AudioDB.getYTLink import getYTLink
-from LyricsOvh.getLyricsSong import getLyricsSong
+from AudioDB import AudioDB
 
 app = FastAPI()
 
@@ -18,16 +14,16 @@ def read_root() -> dict:
 
 @app.get("/random/{name_artist}")
 def getRandomTrackArtist(name_artist: str) -> List[dict]:
-    id_artist = getIdArtist(name_artist)
-    list_id_albums = getIdDiscography(id_artist)
+    id_artist = AudioDB.getIdArtist(name_artist)
+    list_id_albums = AudioDB.getIdDiscography(id_artist)
     list_tracks = []
     for id_album in list_id_albums:
-        list_tracks = list_tracks + getTracksAlbum(id_album)
+        list_tracks = list_tracks + AudioDB.getTracksAlbum(id_album)
     for track in list_tracks:
         track["name_artist"] = name_artist
-        track["suggested_youtube_url"] = getYTLink(
+        track["suggested_youtube_url"] = AudioDB.getYTLink(
             id_artist, track["id_track"])
-        track["lyrics"] = getLyricsSong(name_artist, track["title"])
+        track["lyrics"] = AudioDB.getLyricsSong(name_artist, track["title"])
         del track["id_track"]
     random_number = random.randint(0, len(list_tracks) - 1)
     return list_tracks[random_number]
