f0c6d5bc20394271446be70a85216060dbbc6d84
DonMako <80451849+DonMako@users.noreply.github.com>
2025-11-09 22:14:07 +0100

    Put getYTLink into AudioDB.py

--- APIMusicale/AudioDB.py
+++ APIMusicale/AudioDB.py
@@ -32,3 +32,15 @@ def getTracksAlbum(id_album: int) -> List[dict]:
         dico["title"] = track["strTrack"]
         list_tracks.append(dico)
     return list_tracks
+    
+def getYTLink(id_artist: int, id_track: int) -> str:
+    requete = requests.get(
+        "https://theaudiodb.com/api/v1/json/{APIKEY}/mvid.php?i=" + str(
+            id_artist)
+    ).json()
+    liste_liens = requete["mvids"]
+    for lien in liste_liens:
+        if lien["idTrack"] == id_track:
+            return lien["strMusicVid"]
+        else:
+            return "Pas de lien YT pour cette musique"
