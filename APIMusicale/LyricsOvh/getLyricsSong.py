b6bd4cb6d0237a6e3a3992b01027051fcb55f858
DonMako <80451849+DonMako@users.noreply.github.com>
2025-11-10 12:20:49 +0100

    Delete APIMusicale/LyricsOvh directory

--- APIMusicale/LyricsOvh/getLyricsSong.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-
-def getLyricsSong(name_artist: str, title: str) -> str:
-    requete = requests.get(
-        "https://api.lyrics.ovh/v1/" + name_artist + "/" + title
-    ).json()
-    return requete["lyrics"]
